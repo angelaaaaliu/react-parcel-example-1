4d3f7dd9a95a7572be622e2ca5e250326a7382dc
Angela Liu <liu.angela.1@gmail.com>
2021-04-28 18:29:41 -0700

    loading weather data

--- src/App.js
+++ src/App.js
@@ -1,29 +1,20 @@
 import React from "react";
+import {csv} from "d3-fetch";
 
 const viewHeight = 500;
 const viewWidth = 500;
 
 const App = () => {
+csv(
+        "https://raw.githubusercontent.com/colinmegill/react-parcel-starter/main/weather.csv"
+        )
+    .then(data => console.log(data));
+
     return (
-        <svg 
-        style={{
-            border: "1px solid lightgrey",
-            width: viewWidth,
-            height: viewHeight
-        }}
-        >
-    <circle cx={20} cy={20} r="5" />
-    <rect x={200} y={200} width={10} height={10} fill="rgb(230,230,230)" />
-    <rect x={212} y={200} width={10} height={10} fill="rgb(230,230,230)" />
-    <rect x={224} y={200} width={10} height={10} fill="rgb(230,230,230)" />
-    <rect x={236} y={200} width={10} height={10} fill="rgb(230,230,230)" />
-    <rect x={248} y={200} width={10} height={10} />
-    <line x1={20} y1={viewHeight - 20} x2={150} y2={100} stroke="black" />
-    <text x="20" y="35" style={{ font: "italic 16px sans- serif" }}>
-        Price history of 100 randomly selected Pokemon cards
-    </text>
-    <p>class on April 13th</p>
-    </svg>
+        <div>
+            <h1>Exploratory Data Analysis, Assignment 2, INFO 474 SP 2021</h1>
+            <p>Data!</p>
+        </div>
     );
 };
 
